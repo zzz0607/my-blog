afe725ae6a58c0141978d7e255ee3c2239ec468f
zzz0607 <118792706+zzz0607@users.noreply.github.com>
2026-02-25 09:38:23 +0800

    fix: use SUPABASE_PROJECT_URL and SUPABASE_PUBLISHABLE_API_KEY

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient, SupabaseClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.SUPABASE_PROJECT_URL;
+const supabaseKey = process.env.SUPABASE_PUBLISHABLE_API_KEY;
 
 export const supabase: SupabaseClient | null = 
   (supabaseUrl && supabaseKey) 
